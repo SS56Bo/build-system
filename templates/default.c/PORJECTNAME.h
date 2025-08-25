5803196ecbb5bab5da82f3e69476fb6732b9cea8
SS56Bo <sohamsarkar345@gmail.com>
2025-08-25 10:37:43 +0530

    Project Templates

---
+++ templates/default.c/PORJECTNAME.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <stdint.h>
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
