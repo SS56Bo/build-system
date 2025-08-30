08c977df2da5f17989bcad992ae0474cf684b3fb
SS56Bo <sohamsarkar345@gmail.com>
2025-08-31 00:47:11 +0530

    Wrote bash script

--- bashbuild.sh
+++ bashbuild.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
 
+usage() {
+    >&2 echo "Usage: $0 <project>"
+    return 0
+}
+
 pname=$1
-tname=/var/bashbuild.rc/templates
+pdir="./${pname}"
+tname=/home/soham/Desktop/BS/bashbuild.rc/templates
+#tname = /var/bashbuild.rc/templates
+
+if [ -z "$pname" ]; then
+    usage
+    exit 1
+elif ! [ -d "$tname" ]; then
+    >&2 echo "Unable to find template dir: $tname"
+    exit 2
+elif [ -d "$pdir" ]; then
+    >&2 echo "Project dir already exists: $pdir"
+    exit 3
+fi
+
+cur="$PWD"
+cd $tname
+echo "Select a template:"
+select x in *; do
+    template = "$x"
+    break
+done
+
+cd $cur
+cp -R ${tname}/$template $pdir
+cd $pdir
+for x in *; do
+    new=$(sed "s,PROJECTNAME,$pname,g" <<< "$x")
+    sed "s,PROJECTNAME,$pname,g" < $x > $new
+    if [ -e "$new" ]; then
+        rm -f $x
+    fi
+done 
